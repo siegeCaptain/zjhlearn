24b96e66b8a49817033328aec24b5dc55c07933b
jiahao.zhang <jiahao.zhang@boldseas.com>
2016-10-17 15:02:34 +0800

    初始化spring配置中心项目

---
+++ spring-cloud-config/spring-cloud-config-client/src/main/java/org/springframework/cloud/config/client/ConfigClientAutoConfiguration.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2013-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.cloud.config.client;
+
+import org.springframework.beans.factory.BeanFactoryUtils;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+/**
+ * Expose a ConfigClientProperties just so that there is a way to inspect the properties
+ * bound to it. It won't be available in time for autowiring into the bootstrap context,
+ * but the values in this properties object will be the same as the ones used to bind to
+ * the config server, if there is one.
+ *
+ * @author Dave Syer
+ *
+ */
+@Configuration
+public class ConfigClientAutoConfiguration {
+
+	@Bean
+	public ConfigClientProperties configClientProperties(Environment environment,
+			ApplicationContext context) {
+		if (context.getParent() != null
+				&& BeanFactoryUtils.beanNamesForTypeIncludingAncestors(
+						context.getParent(), ConfigClientProperties.class).length > 0) {
+			return BeanFactoryUtils.beanOfType(context.getParent(),
+					ConfigClientProperties.class);
+		}
+		ConfigClientProperties client = new ConfigClientProperties(environment);
+		return client;
+	}
+
+	@Configuration
+	@ConditionalOnClass(HealthIndicator.class)
+	@ConditionalOnBean(ConfigServicePropertySourceLocator.class)
+	@ConditionalOnProperty(value = "health.config.enabled", matchIfMissing = true)
+	protected static class ConfigServerHealthIndicatorConfiguration {
+
+		@Bean
+		public ConfigServerHealthIndicator configServerHealthIndicator(
+				ConfigServicePropertySourceLocator locator) {
+			return new ConfigServerHealthIndicator(locator);
+		}
+	}
+
+	@ConfigurationProperties("health.config")
+	public static class Health {
+		/**
+		 * Flag to inidicate that the config server health indicator should be installed. 
+		 */
+		boolean enabled;
+
+		public boolean isEnabled() {
+			return enabled;
+		}
+
+		public void setEnabled(boolean enabled) {
+			this.enabled = enabled;
+		}
+	}
+
+}
---
+++ spring-cloud-config/spring-cloud-config-client/src/main/java/org/springframework/cloud/config/environment/Environment.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2013-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.config.environment;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Simple plain text serializable encapsulation of a list of property sources. Basically a
+ * DTO for {@link org.springframework.core.env.Environment}, but also applicable outside
+ * the domain of a Spring application.
+ * 
+ * @author Dave Syer
+ *
+ */
+public class Environment {
+
+	private String name;
+
+	private String[] profiles = new String[0];
+
+	private String label;
+
+	private List<PropertySource> propertySources = new ArrayList<PropertySource>();
+
+	public Environment(String name, String... profiles) {
+		this(name, profiles, "master");
+	}
+
+	@JsonCreator
+	public Environment(@JsonProperty("name") String name,
+			@JsonProperty("profiles") String[] profiles,
+			@JsonProperty("label") String label) {
+		super();
+		this.name = name;
+		this.profiles = profiles;
+		this.label = label;
+	}
+
+	public void add(PropertySource propertySource) {
+		this.propertySources.add(propertySource);
+	}
+
+	public void addFirst(PropertySource propertySource) {
+		this.propertySources.add(0, propertySource);
+	}
+
+	public List<PropertySource> getPropertySources() {
+		return propertySources;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+	
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	public String[] getProfiles() {
+		return profiles;
+	}
+
+	public void setProfiles(String[] profiles) {
+		this.profiles = profiles;
+	}
+
+	@Override
+	public String toString() {
+		return "Environment [name=" + name + ", profiles=" + Arrays.asList(profiles)
+				+ ", label=" + label + ", propertySources=" + propertySources + "]";
+	}
+
+}
